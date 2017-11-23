6429c55cf2d30c9efa277f0c33a644aa9fc83180
Silvamorta <sagtanas94@mail.ru>
2017-11-23 15:08:49 +0500

    add: миграции для записок

---
+++ database/migrations/2017_11_23_075209_create_notes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('note_id')->unsigned();
+            $table->foreign('note_id')
+                ->references('id')->on('notes_list')
+                ->onDelete('cascade');;
+            $table->string('type');
+            $table->string('heading')->nullable();
+            $table->text('content');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
