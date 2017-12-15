82a4ce921ed8f2e283154aa27bda864804e12090
Silvamorta <sagtanas94@mail.ru>
2017-12-15 14:32:00 +0500

    fix: размер изображения

--- resources/views/notes/note/elements/picture.blade.php
+++ resources/views/notes/note/elements/picture.blade.php
@@ -1,6 +1,6 @@
 <div class="note-block__element note-picture">
     <div class="note-picture__content">
-        <img src="{{ Storage::disk('s3')->url($content) }}" alt="O-o-ops">
+        <img src="{{ Storage::disk('s3')->url($content) }}" alt="O-o-ops" style="max-width: 100%">
     </div>
     @if (filled($heading))
         <div class="note-picture__heading">{{ $heading }}</div>
